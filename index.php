2bb28528e000d440f54b2bf2eea24413f6d70c95
coelho_matheus <68906875+MatheusCoelho13@users.noreply.github.com>
2024-02-28 22:21:01 -0300

    primeira versao
    opa 1 versao de muitas 
    hello word

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./src/css/style.css"/>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Slab:ital,wght@0,100..700;1,100..700&family=Joti+One&family=Julius+Sans+One&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+<title>
+   opa
+  </title>
+</head>
+<body>
+<h1 id="h1_title">Bem vindo ao site de sorteio do COC Terceirão 2024</h1>
+<div class="img">
+    <img src="./src/assets/logologo.svg" alt="coc" id="img_logo">
+</div>
+<div class="menu_rank">
+    <?
+?>
+<h2 id="menu_h2"> Top compradores</h2>
+</div>
+<a href="./src/pages/login.php" class="login_a">Login</a>
+</body>
+</html>
+    
